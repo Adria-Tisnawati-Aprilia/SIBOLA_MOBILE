145ec69519675abd85cb87a8ef6c4d16c0a95d36
Adria-Tisnawati-Aprilia <adriatisnawatiaprilia@gmail.com>
2022-05-15 11:41:58 +0700

    LOGIN

--- sibola/lib/screens/splash_screens.dart
+++ sibola/lib/screens/splash_screens.dart
@@ -7,13 +7,14 @@ import 'package:sibola/auth/login.dart';
 import 'homepage.dart';
 
 class SplashScreens extends StatefulWidget {
-  const SplashScreens({ Key? key }) : super(key: key);
+  const SplashScreens({Key? key}) : super(key: key);
 
   @override
   State<SplashScreens> createState() => _SplashScreensState();
 }
 
 class _SplashScreensState extends State<SplashScreens> {
+  @override
   void initState() {
     super.initState();
     cek();
@@ -24,11 +25,13 @@ class _SplashScreensState extends State<SplashScreens> {
     var loading = const Duration(seconds: 3);
     String? value = pref.getString("login");
     return Timer(loading, () {
-      if (value != null){
-      Navigator.of(context).pushAndRemoveUntil(
-          MaterialPageRoute(builder: (context) => HomePage()), (route) => false);
-      }else{
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+      if (value != null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => HomePage()),
+            (route) => false);
+      } else {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => LoginPage()));
       }
     });
   }
@@ -37,13 +40,15 @@ class _SplashScreensState extends State<SplashScreens> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-      width: double.infinity,
-      height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topRight,
-            colors: [Color(000000), Color(0xFFF1E6FF),
-            ],
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topRight,
+              colors: [
+                Color(000000),
+                Color(0xFFF1E6FF),
+              ],
             ),
           ),
           child: Column(
@@ -52,14 +57,15 @@ class _SplashScreensState extends State<SplashScreens> {
             children: [
               Column(
                 children: [
-                  Image.asset('assets/images/logo.png',
+                  Image.asset(
+                    'assets/images/logo.png',
                     height: 300.0,
                     width: 300.0,
                   ),
                   Text(
                     "Sistem Informasi Booking Lapangan Indramayu",
                     textAlign: TextAlign.center,
-                    style:TextStyle(
+                    style: TextStyle(
                       color: Colors.white,
                       fontWeight: FontWeight.bold,
                       fontSize: 18.0,
@@ -67,11 +73,9 @@ class _SplashScreensState extends State<SplashScreens> {
                   ),
                 ],
               ),
-              
               CircularProgressIndicator(),
             ],
-          )
-      ),
+          )),
     );
   }
 }
